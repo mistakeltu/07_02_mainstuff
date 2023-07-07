c783b80b1f91f8832f7e2f6673cc853354215bb7
Karolis <karolisdarbinis2@gmail.com>
2023-07-07 12:15:03 +0300

    Update

---
+++ modalWindow/modalWindow.js
@@ -0,0 +1,32 @@
+console.clear();
+
+const showModalBtnDOM = document.querySelectorAll('.show-modal');
+const modalDOM = document.querySelector('.modal');
+const hiddenDOM = document.querySelector('.hidden');
+const closeModalDOM = document.querySelector('.close-modal');
+const overlayDOM = document.querySelector('.overlay');
+
+const closeFunction = _ => {
+  hiddenDOM.classList.add('hidden');
+  overlayDOM.classList.add('hidden');
+};
+
+const openFunction = _ => {
+  modalDOM.classList.remove('hidden');
+  overlayDOM.classList.remove('hidden');
+};
+
+for (let i = 0; i < showModalBtnDOM.length; i++) {
+  showModalBtnDOM[i].addEventListener('click', openFunction);
+}
+
+closeModalDOM.addEventListener('click', closeFunction);
+overlayDOM.addEventListener('click', closeFunction);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    if (!modalDOM.classList.contains('hidden')) {
+      closeFunction();
+    }
+  }
+});
