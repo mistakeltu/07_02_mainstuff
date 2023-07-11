2733d965cd597a6a1ee737849e5d1ac4fd84bac9
Karolis <karolisdarbinis2@gmail.com>
2023-07-11 11:06:23 +0300

    Update

--- this/script.js
+++ this/script.js
@@ -2,35 +2,78 @@
 console.clear();
 
 // The this Keyword in Practice
-console.log(this);
+// console.log(this);
 
-const calcAge = function (birthYear) {
-  console.log(2037 - birthYear);
-  console.log(this);
-};
-calcAge(1991);
+// const calcAge = function (birthYear) {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+// calcAge(1991);
 
-const calcAgeArrow = birthYear => {
-  console.log(2037 - birthYear);
-  console.log(this);
-};
-calcAgeArrow(1980);
+// const calcAgeArrow = birthYear => {
+//   console.log(2037 - birthYear);
+//   console.log(this);
+// };
+// calcAgeArrow(1980);
 
-const jonas = {
-  year: 1991,
+// const jonas = {
+//   year: 1991,
+//   calcAge: function () {
+//     console.log(this);
+//     console.log(2037 - this.year);
+//   },
+// };
+// jonas.calcAge();
+
+// const matilda = {
+//   year: 2017,
+// };
+
+// matilda.calcAge = jonas.calcAge;
+// matilda.calcAge();
+
+// const f = jonas.calcAge;
+// f();
+
+//var firstName = 'kazkas'; is cia paimtu arrow f-ja reiksme
+
+const karolis = {
+  firstName: 'karolis',
+  year: 2000,
   calcAge: function () {
-    console.log(this);
-    console.log(2037 - this.year);
+    // console.log(this);
+    console.log(2023 - this.year);
+
+    //Solution 1
+    // const self = this; //self or that
+    // const isMillenial = function () {
+    //   console.log(self);
+    //   console.log(self.year >= 1981 && self.year <= 1996);
+    //   // console.log(this.year >= 1981 && this.year <= 1996);
+    // };
+
+    //Solution 2
+    const isMillenial = () => {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+    isMillenial();
   },
+
+  // greet: () => {
+  //   console.log(this);
+  //   console.log(`Hey, ${this.firstName}`);
+  // },
 };
-jonas.calcAge();
 
-const matilda = {
-  year: 2017,
+karolis.calcAge();
+// karolis.greet();
+
+//arguments keyword
+
+const addExpr = function (a, b) {
+  console.log(arguments);
+  return a + b;
 };
-
-matilda.calcAge = jonas.calcAge;
-matilda.calcAge();
-
-const f = jonas.calcAge;
-f();
+addExpr(2, 5);
+addExpr(2, 5, 10, 5);
